39e30300d772abea47169542ee343e5d6e62daee
xRaidersito <raidgames123@gmail.com>
2021-04-15 10:51:34 -0400

    added example gui

---
+++ src/main/java/me/larux/lsupport/menu/ExampleMenu.java
@@ -0,0 +1,28 @@
+package me.larux.lsupport.menu;
+
+import me.larux.lsupport.gui.Gui;
+import me.larux.lsupport.gui.SimpleGui;
+import me.larux.lsupport.gui.SimpleGuiPage;
+import me.larux.lsupport.gui.item.SimpleGuiItem;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class ExampleMenu {
+
+    public void openMenuToPlayer(Player player) {
+
+        Gui gui = new SimpleGui.Builder()
+                .addPage(
+                        new SimpleGuiPage.Builder("test menu", 27)
+                                .addItem(new SimpleGuiItem.Builder()
+                                        .slot(4)
+                                        .item(new ItemStack(Material.COAL))
+                                        .build())
+                                .build())
+                .build();
+
+        gui.openMenu(player);
+    }
+
+}
